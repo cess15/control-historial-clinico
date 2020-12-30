31c9c9b5f94bb668fd93ddde593d87364fc62019
Cesar <cesarlata1@hotmail.com>
2020-12-29 19:12:56 -0500

    Feature login (#2)
    * Landing page
    
    * Feature Login
    
    * login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/admin', 'HomeController@index')->name('home');
